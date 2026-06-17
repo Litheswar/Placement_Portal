42a4e4d7bcbd552e5da58a321f4e165c4afdcc5f
Litheswar <litheswar19@gmail.com>
2026-06-17 23:55:27 +0530

    M2 Completed

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    SECRET_KEY = "placement_portal_flask_secret_key"
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'placement.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
